3889be3a93f371a8d243b7fdca0c06ef18cc2bfd
Jordan <jordanashment@gmail.com>
2021-11-09 22:05:58 -0700

    Installable shell script for some useful tools

---
+++ Installable/macEnv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Xcode command line tools
+sudo xcode-select --install
+
+# homebrew
+curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+# nvm
+brew install nvm
+
+# python3
+brew install python
+
+# COMMAND LINE TOOLS
+
+# bat
+brew install bat
+
+# autojump
+brew install autojump
